8a75365170791a4219ba8d047de1f11efcb28834
iMarishin <move_up@icloud.com>
2021-07-03 18:00:46 +0500

    Initial Commit

---
+++ VitaMi/VitaMiApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaMiApp.swift
+//  VitaMi
+//
+//  Created by Иван Маришин on 03.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct VitaMiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
