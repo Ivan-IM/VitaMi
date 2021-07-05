9d028398c903a32e5b06a52504780ab450e44e11
iMarishin <move_up@icloud.com>
2021-07-05 22:26:42 +0500

    some design fix

--- VitaMi/CustomView/SymptomsBottomView.swift
+++ VitaMi/CustomView/SymptomsBottomView.swift
@@ -15,7 +15,7 @@ struct SymptomsBottomView: View {
     
     var body: some View {
         ZStack {
-            Color.blue.contrast(10).opacity(0.9)
+            Color.blue.contrast(10).opacity(0.8)
                 .cornerRadius(10)
                 .shadow(color: Color.black.opacity(0.9), radius: 5, x: 0, y: 0)
             
@@ -26,37 +26,33 @@ struct SymptomsBottomView: View {
                     Image(systemName: "chevron.left.circle")
                         .foregroundColor(Color.white)
                     Text("Cancel")
+                        .font(.headline)
+                        .fontWeight(.bold)
                         .foregroundColor(Color.white)
                 })
-                .frame(width: 100, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .background(Color.clear.opacity(0.8))
-                .cornerRadius(10)
                 Spacer()
                 Button(action: {
                     showingAlert = true
                 }, label: {
                     Text("Clear")
+                        .font(.headline)
+                        .fontWeight(.bold)
                         .foregroundColor(Color.white)
                        })
                 .alert(isPresented: $showingAlert) {
                     Alert(title: Text("Clear the list"), message: Text("Are you sure?"), primaryButton: .destructive(Text("Ok"), action: action), secondaryButton: .cancel())
                 }
-                    .frame(width: 80, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                    .background(Color.clear.opacity(0.8))
-                    .cornerRadius(10)
-                    .shadow(color: Color.black.opacity(0.7), radius: 5, x: 0, y: 0)
                 Spacer()
                 NavigationLink(
                     destination: ResultView(),
                     label: {
                         Text("Save")
+                            .font(.headline)
+                            .fontWeight(.bold)
                             .foregroundColor(Color.white)
                         Image(systemName: "chevron.right.circle")
                             .foregroundColor(Color.white)
                 })
-                .frame(width: 100, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                .background(Color.clear.opacity(0.9))
-                .cornerRadius(10)
             }
             .padding(.horizontal, 10.0)
         }
