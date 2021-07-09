5c5ca641db5be07345a8efd9d559a16bf76ccfb6
iMarishin <move_up@icloud.com>
2021-07-09 20:47:19 +0500

    fix save and init algorithm

--- VitaMi/View/ResultTestingView.swift
+++ VitaMi/View/ResultTestingView.swift
@@ -1,5 +1,5 @@
 //
-//  SymptomsListView.swift
+//  ResultTestingView.swift
 //  VitaMi
 //
 //  Created by Иван Маришин on 09.07.2021.
@@ -7,54 +7,48 @@
 
 import SwiftUI
 
-struct SymptomsListView: View {
+struct ResultTestingView: View {
     
     @EnvironmentObject var user: User
     
     var body: some View {
         ScrollView(showsIndicators: false) {
-            ForEach(Symtom.getSymptomsList()) { symptom in
-                CustomCellSymptomsListView(title: symptom.ruName, isSelected: user.symptomsList.contains(symptom.enName)) {
-                    if user.symptomsList.contains(symptom.enName) {
-                        user.symptomsList.removeAll(where: { $0 == symptom.enName })
+            ForEach(Element.getElementsList()) { element in
+                if user.lowElementsList.contains(element.symbol) {
+                    CustomCellResultView(title: element.ruName) {
+                        
                     }
-                    else {
-                        user.symptomsList.append(symptom.enName)
-                    }
+                    .padding(.vertical, 2.0)
+                    .padding(.horizontal, 10.0)
                 }
-                .padding(.vertical, 2.0)
-                .padding(.horizontal, 10.0)
             }
         }
+        
     }
 }
 
-struct SymptomsListView_Previews: PreviewProvider {
+struct ResultTestingView_Previews: PreviewProvider {
     static var previews: some View {
-        SymptomsListView()
+        ResultTestingView()
     }
 }
 
-struct CustomCellSymptomsListView: View {
+struct CustomCellResultView: View {
     
     var title: String
-    var isSelected: Bool
     var action: () -> Void
     
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).cornerRadius(20).opacity(0.8)
             .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.7), radius: 3, x: 0, y: 0)
-
+            
             Button(action: self.action) {
                 HStack {
                     Text(self.title)
                         .foregroundColor(Color.black)
                     Spacer()
-                    if self.isSelected {
-                        Image(systemName: "checkmark")
-                            .foregroundColor(Color.black)
-                    }
+                    Image(systemName: "questionmark.square")
                 }
                 .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
             }
--- VitaMi/View/TestingView.swift
+++ VitaMi/View/TestingView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 
 struct TestingView: View {
+    
+    @EnvironmentObject var user: User
+    @State private var showingAlert = false
+    
     var body: some View {
         TabView {
             SymptomsListView()
+                .onDisappear(){
+                    UserDefaults.standard.set(user.symptomsList, forKey: "SymptomsList")
+                    user.lowElementsList.removeAll()
+                    user.elementsFilterAlgorinm()
+                }
                 .tabItem {
                     Image(systemName: "house.fill")
                     Text("Symptoms")
@@ -25,6 +34,18 @@ struct TestingView: View {
                     Image(systemName: "house.fill")
                     Text("BioAnalysis")
                 }
+        }
+        .navigationTitle("Testing")
+        .navigationBarItems(trailing: Button(action: {
+            showingAlert.toggle()
+        }, label: {
+            Text("Clear")
+        })).alert(isPresented: $showingAlert) {
+            Alert(title: Text("Clear"), message: Text("Are you want clear symptoms list?"), primaryButton: .destructive(Text("Ok"), action: {
+                user.symptomsList.removeAll()
+                user.lowElementsList.removeAll()
+                UserDefaults.standard.set(user.symptomsList, forKey: "SymptomsList")
+            }), secondaryButton: .cancel())
         }
     }
 }
