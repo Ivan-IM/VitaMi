461199ea5eba3b392eb7b702b120d0af998715bf
iMarishin <move_up@icloud.com>
2021-07-15 22:07:13 +0500

    some struct fix

--- VitaMi/View/BiochemicalAnalysisView.swift
+++ VitaMi/View/BiochemicalAnalysisView.swift
@@ -15,7 +15,18 @@ struct BiochemicalAnalysisView: View {
         ScrollView(showsIndicators: false) {
             ForEach(Element.getElementsList()) { element in
                 if user.lowElementsList.contains(element.symbol) {
-                    CustomCellBioAnalysisView(title: element.ruName)
+                    ZStack {
+                        Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).cornerRadius(20).opacity(0.8)
+                            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.7), radius: 3, x: 0, y: 0)
+                        
+                        HStack {
+                            Text(element.ruName)
+                                .foregroundColor(Color.black)
+                            
+                        }
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                    }
+                    .frame(height: 40)
                     .padding(.vertical, 2.0)
                     .padding(.horizontal, 10.0)
                 }
@@ -27,26 +38,5 @@ struct BiochemicalAnalysisView: View {
 struct BiochemicalAnalysisView_Previews: PreviewProvider {
     static var previews: some View {
         BiochemicalAnalysisView()
-    }
-}
-
-struct CustomCellBioAnalysisView: View {
-    
-    var title: String
-
-    
-    var body: some View {
-        ZStack {
-            Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).cornerRadius(20).opacity(0.8)
-            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.7), radius: 3, x: 0, y: 0)
-            
-                HStack {
-                    Text(self.title)
-                        .foregroundColor(Color.black)
-                       
-                }
-                .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-        }
-        .frame(height: 40)
     }
 }
--- VitaMi/View/ResultTestingView.swift
+++ VitaMi/View/ResultTestingView.swift
@@ -15,9 +15,20 @@ struct ResultTestingView: View {
         ScrollView(showsIndicators: false) {
             ForEach(Element.getElementsList()) { element in
                 if user.lowElementsList.contains(element.symbol) {
-                    CustomCellResultView(title: element.ruName) {
-                        
+                    ZStack {
+                        Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).cornerRadius(20).opacity(0.8)
+                            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.7), radius: 3, x: 0, y: 0)
+                        Button(action: {}) {
+                            HStack {
+                                Text(element.ruName)
+                                    .foregroundColor(Color.black)
+                                Spacer()
+                                Image(systemName: "questionmark.circle")
+                            }
+                            .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        }
                     }
+                    .frame(height: 40)
                     .padding(.vertical, 2.0)
                     .padding(.horizontal, 10.0)
                 }
@@ -29,29 +40,5 @@ struct ResultTestingView: View {
 struct ResultTestingView_Previews: PreviewProvider {
     static var previews: some View {
         ResultTestingView()
-    }
-}
-
-struct CustomCellResultView: View {
-    
-    var title: String
-    var action: () -> Void
-    
-    var body: some View {
-        ZStack {
-            Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)).cornerRadius(20).opacity(0.8)
-            .shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.7), radius: 3, x: 0, y: 0)
-            
-            Button(action: self.action) {
-                HStack {
-                    Text(self.title)
-                        .foregroundColor(Color.black)
-                    Spacer()
-                    Image(systemName: "questionmark.circle")
-                }
-                .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
-            }
-        }
-        .frame(height: 40)
     }
 }
