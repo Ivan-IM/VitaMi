6e206164bf66b3f465a57660ec30726a5ba2c6ef
iMarishin <move_up@icloud.com>
2021-10-06 14:49:55 +0500

    add fb

--- VitaMi/VitaMiApp.swift
+++ VitaMi/VitaMiApp.swift
@@ -5,10 +5,15 @@
 //  Created by Иван Маришин on 03.07.2021.
 //
 
+import Firebase
+import FirebaseAuth
 import SwiftUI
 
 @main
 struct VitaMiApp: App {
+    init() {
+            FirebaseApp.configure()
+        }
     var body: some Scene {
         WindowGroup {
             ContentView()
